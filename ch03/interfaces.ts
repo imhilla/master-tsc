ed16c00b4bc71f26ed554e18f112b4dfd9989965
imhilla <hillaryodhiambo282@gmail.com>
2022-05-05 21:03:55 +0300

    use in operator

--- ch03/interfaces.ts
+++ ch03/interfaces.ts
@@ -30,3 +30,23 @@ let weakTypeNoOverlap: IWeakType = {
   description: "a description",
 };
 // the in operator
+interface IIdName {
+  id: number;
+  name: string;
+}
+
+interface IDescrValue {
+  descr: string;
+  value: number;
+}
+
+function printNameOrValue(obj: IIdName | IDescrValue): void {
+  if ("id" in obj) {
+    console.log(`obj.name${obj.name}`);
+  }
+  if ("descr" in obj) {
+    console.log(`obj.descr${obj.descr}`);
+  }
+}
+printNameOrValue({ id: 1, name: "nameValue" });
+printNameOrValue({ descr: "description", value: 2 });
