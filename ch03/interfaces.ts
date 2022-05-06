bac5cc5d231351898d3b97957a6d6e127c36e43f
imhilla <hillaryodhiambo282@gmail.com>
2022-05-06 05:33:40 +0300

    add multiple interfacs

--- ch03/interfaces.ts
+++ ch03/interfaces.ts
@@ -68,3 +68,80 @@ getProperty("id", { id: 1, name: "first name" });
 getProperty("name", { id: 2, name: "seconn" });
 
 // classes
+class SimpleClass {
+  id: number | undefined;
+  print(): void {
+    console.log(`SimpleClass.print() called ${this.id}`);
+  }
+}
+let mySimpleClass = new SimpleClass();
+mySimpleClass.print();
+
+//implementing interfaces
+
+interface IPrint {
+  print(): void;
+}
+class ClassA implements IPrint {
+  print(): void {
+    console.log(`ClassA.print() called.`);
+  }
+}
+class ClassB implements IPrint {
+  print(): void {
+    console.log(`ClassB.print() called`);
+  }
+}
+
+let classA = new ClassA();
+let classB = new ClassB();
+
+function printClass(a: IPrint) {
+  a.print;
+}
+
+printClass(classA);
+printClass(classB);
+// class constructors
+
+class ClassWithConstructor {
+  id: number;
+  constructor(id: number) {
+    this.id = id;
+  }
+}
+
+let classWithConstructor = new ClassWithConstructor(10);
+console.log(`classWithConstructor = ${JSON.stringify(classWithConstructor)}`);
+// class modifiers
+class ClassEs6Private {
+  #id: number;
+  constructor(id: number) {
+    this.#id = id;
+  }
+}
+
+let es6PrivateClass = new ClassEs6Private(10);
+interface IBase {}
+interface IDerivedFromBase {}
+
+class BaseClass implements IBase {
+  id: number = 0;
+}
+class DerivedFromBaseClass extends BaseClass implements IDerivedFromBase {
+  name: string = "Namestring";
+}
+
+//implementing multiple interfaces
+interface IFirstInterface {
+  id: number;
+}
+
+interface ISecondInterface {
+  name: string;
+}
+
+class MultipleInterfaces implements IFirstInterface, ISecondInterface {
+  id: number = 0;
+  name: string = "name string";
+}
