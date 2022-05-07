15ce229a65ab9209bca60dc5ef7490261dbb2504
imhilla <hillaryodhiambo282@gmail.com>
2022-05-07 12:52:14 +0300

    update app

--- ch03/interfaces.ts
+++ ch03/interfaces.ts
@@ -495,3 +495,20 @@ type inferredFromFnParam<T> = T extends (a: infer U) => void ? U : never;
 function testInferredFromParam<T>(arg: inferredFromFnParam<T>) {}
 testInferredFromParam<(a: number) => void>(1);
 testInferredFromParam<(a: string) => void>("test");
+
+type inferredFromFnReturnType<T> = T extends (a: string) => infer U ? U : never;
+function testInferredFromReturnType<T>(arg: inferredFromFnReturnType<T>) {}
+testInferredFromReturnType<(a: string) => number>(1);
+testInferredFromReturnType<(a: string) => boolean>(false);
+
+/**
+ * Type inference from arrays
+ * **/
+type inferredTypeFromArray<T> = T extends (infer U)[] ? U : never;
+function testInferredFromArray<T>(args: inferredTypeFromArray<T>) {}
+testInferredFromArray<string[]>("test");
+testInferredFromArray<number[]>(1);
+
+/**
+ * standard conditional types
+ **/
