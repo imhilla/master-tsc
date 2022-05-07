a2da6cc4d2081f9a85390ee6b0cface28f2d9f4f
imhilla <hillaryodhiambo282@gmail.com>
2022-05-07 11:21:20 +0300

    update get tuple sting abc

--- ch03/interfaces.ts
+++ ch03/interfaces.ts
@@ -405,3 +405,54 @@ let recordedCdVar: RecordedCd = {
   c: 1,
   d: 3,
 };
+
+/**
+ * conditional types
+ **/
+type NumberOrString<T> = T extends number ? number : string;
+
+function logNumberOrString<T>(input: NumberOrString<T>) {
+  console.log(`logNmunerOrString: ${input}`);
+}
+
+logNumberOrString<number>(1);
+logNumberOrString<string>("test");
+// logNumberOrString<boolean>(true);
+
+/**
+ * conditional type chaining
+ * **/
+interface IA {
+  a: number;
+}
+interface IAb {
+  a: number;
+  b: string;
+}
+
+interface IAbc {
+  a: number;
+  b: string;
+  c: boolean;
+}
+
+type abc_ab_a<T> = T extends IAbc
+  ? [number, string, boolean]
+  : T extends IAb
+  ? [number, string]
+  : T extends IA
+  ? [number]
+  : never;
+
+function getTupleStringAbc<T>(tupleValue: abc_ab_a<T>): string {
+  let [...tupleDestructured] = tupleValue;
+  let returnString = "|";
+  for (let value of tupleDestructured) {
+    returnString += `${value}`;
+  }
+  return returnString;
+}
+
+let keyA = getTupleStringAbc<IA>([1]);
+let keyB = getTupleStringAbc<IAb>([1, "test"]);
+let keyC = getTupleStringAbc<IAbc>([1, "test", true]);
