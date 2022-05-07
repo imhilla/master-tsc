7245b49dc62e122a2ecd68a14dd05d9d24240d0c
imhilla <hillaryodhiambo282@gmail.com>
2022-05-07 13:05:32 +0300

    finalize generic

--- ch03/interfaces.ts
+++ ch03/interfaces.ts
@@ -512,3 +512,13 @@ testInferredFromArray<number[]>(1);
 /**
  * standard conditional types
  **/
+type ExcludeStringAndNumber = Exclude<
+  string | number | boolean,
+  string | number
+>;
+let boolValue: ExcludeStringAndNumber = true;
+type StringOrNumber = Extract<string | boolean | never, string | number>;
+let stringValue: StringOrNumber = "test";
+
+type NotNullOrUndef = NonNullable<number | undefined | null>;
+let numValue: NotNullOrUndef = 1;
